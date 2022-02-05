ad113aa8115c379b4a1847b5c02eb3ddd9d94575
subhabratasaha <subha94u@yahoo.co.in>
2022-02-05 23:19:02 +0530

    Include %

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location');
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + ' degree')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + ' %')
         }
     })
 }
